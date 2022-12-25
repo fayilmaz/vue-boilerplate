1287086800203fbc4abf9045964465a84c0f0289
fyilmazy <fahrettinyilmaz@gmail.com>
2022-12-26 00:36:01 +0300

    vue setup, vuex, router implemented

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    user: null,
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div>Home Page</div>
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+export default {
+  name: "HomePage",
+  components: {},
+  setup() {
+    const store = useStore();
+    return { store };
+  },
+};
+</script>
